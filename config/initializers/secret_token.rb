4b02cf457201dadba86e987c1615648c83e146ea
Carlos Cervantes <CCER562@yahoo.com>
2013-04-25 18:58:34 -0700

    Initial Checkin, nothing but just an empty rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sproutzly::Application.config.secret_token = 'a31d4e4ecf04b222855bd4c2839fde6f7eb1e79a722f390c8cf4f0df4d194c292f859f47f5b43d0c429ad113e8fdaeae517ca1556ad57340dc4c344a0c667df5'
