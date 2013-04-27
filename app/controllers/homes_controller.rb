dbd8a3e2de4d8a288a3c590a377671231b36233c
Carlos Cervantes <CCER562@yahoo.com>
2013-04-27 16:02:21 -0700

    Removed bootstrap from landing page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,7 @@
 class HomesController < ApplicationController
-  layout nil
-
   def index
     respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @homes, :template => nil }
+      format.html { render action: "index", layout: 'blank' }
     end
   end
 end
