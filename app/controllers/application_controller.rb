e2c657e0258ae7607102776b6155203624803cd1
Carlos Cervantes <CCER562@yahoo.com>
2013-04-27 16:46:40 -0700

    Some styling, statrting users, going to try adding rails bootstrap gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+
+  def current_user
+  	User.find(session[:current_user_id]) if session[:current_user_id]
+  end
+
+  
+
 end
