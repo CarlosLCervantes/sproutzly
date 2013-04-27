8c34c2ed8dcd7e85799a8d8c4ed8b3bd8f9f4f91
winston_yee <winsyee@gmail.com>
2013-04-27 15:53:46 -0700

    merged with flat ui

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,10 @@
 class HomesController < ApplicationController
+  layout nil
+
   def index
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @homes }
+      format.json { render json: @homes, :template => nil }
     end
   end
 end
