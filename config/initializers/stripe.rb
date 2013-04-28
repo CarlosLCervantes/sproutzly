ede49530b40e8143f267ced1f298f6fb08ed1373
Carlos Cervantes <CCER562@yahoo.com>
2013-04-28 09:31:11 -0700

    updated ruby version

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = "sk_test_ZKbSZ5d4zZNiYM4A66dyL9Ec"
-STRIPE_PUBLIC_KEY = "pk_test_5270nkRBjazmESxGpg6YES7O"
+# Stripe.api_key = "sk_test_ZKbSZ5d4zZNiYM4A66dyL9Ec"
+# STRIPE_PUBLIC_KEY = "pk_test_5270nkRBjazmESxGpg6YES7O"
