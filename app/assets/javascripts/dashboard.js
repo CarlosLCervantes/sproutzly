2556bd0dfd185c19555740bdbb814389c7476f44
Carlos Cervantes <CCER562@yahoo.com>
2013-04-28 07:27:42 -0700

    bug fixes

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -4,13 +4,11 @@ $(document).ready(function() {
 });
 
 function startAction(e) {
-	//checkPayment();//glob
-	console.log(e);
-
+	//checkPayment();
 	var self = $(this);
-	var parentId = 'div#' + $(e.currentTarget).parent().parent().parent().first().attr('id');
-	var action_item = self.parents(parentId).find("div.action-item");
-	// var action_item = self.parents("div#item-twitter").find("div.action-item");
-	//debug(action_item.length);
+	//var parentId = 'div#' + $(e.currentTarget).parent().parent().parent().first().attr('id');
+	//var action_item = self.parents(parentId).find("div.action-item");
+	var action_item = self.parents("div.work-item").find("div.action-item");
+	debug(action_item.length);
 	action_item.slideDown("slow");
 }
