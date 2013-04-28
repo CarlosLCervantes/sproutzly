e7b88d329b930199f5a594a76e8b60d549537d6d
winston_yee <winsyee@gmail.com>
2013-04-28 06:29:42 -0700

    updated addons

---
+++ app/controllers/addons_controller.rb
@@ -0,0 +1,11 @@
+class AddonsController < ApplicationController
+  # GET /addons
+  # GET /addons.json
+  def index
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+  end
+
+end
---
+++ test/unit/helpers/addons_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class AddonsHelperTest < ActionView::TestCase
+end
